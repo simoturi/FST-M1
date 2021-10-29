40f7aa624eeee6c330c10425d70588033d8311b7
simoturi <93118210+simoturi@users.noreply.github.com>
2021-10-29 12:33:58 +0530

    Add files via upload

---
+++ SQL/Activities/Activity2.sql
@@ -0,0 +1,13 @@
+REM   Script: Activity2
+REM   act2
+
+insert all  
+into salesman values(1001,'Siva','China',100) 
+into salesman values(1002,'Ganesh','Uganda',90) 
+into salesman values(1003,'Reethu','China',90) 
+into salesman values(1004,'Bhavya','pakistan',50) 
+into salesman values(1005,'Chandu','Pakistan',50)  
+select 1 from dual;
+
+select * from salesman;
+

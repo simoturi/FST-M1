49d8d15b4e7c07aba6b3d1f2558fce96bd4fb3df
simoturi <93118210+simoturi@users.noreply.github.com>
2021-12-30 21:53:20 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4.java
@@ -0,0 +1,10 @@
+package com.ibm.FST_Selenium.TestNG;
+
+import org.testng.annotations.Test;
+
+public class Activity4 {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
